128dc67c3aa67653a9c5d99549abfd8169ab0a2e
aglili <cecilselorm34@gmail.com>
2023-07-30 12:54:52 +0000

    transactional endpointts

--- paystackpay/transaction.py
+++ paystackpay/transaction.py
@@ -4,13 +4,13 @@ from .utils import validate_amount
 
 class Transaction(Base):
     
-    def initialize_transaction(self,email:EmailStr,amount):
+    def initialize_transaction(self,email:EmailStr,amount:float):
         endpoint = 'transaction/initialize'
-        validate_amount = validate_amount(amount)
+        validated_amount = validate_amount(amount)*100
 
         data = {
             "email":email,
-            "amount":validate_amount
+            "amount":str(validated_amount)
         }
 
         return self.make_request("POST",endpoint=endpoint,data=data)
@@ -32,4 +32,42 @@ class Transaction(Base):
         return self.make_request("GET",endpoint=endpoint)
     
 
+    def charge_authorization(self,email:EmailStr,amount:float,auth_code:str):
+        endpoint = '/transaction/charge_authorization'
+
+        validated_amount = validate_amount(amount=amount)*100
+
+        data = {
+            "email":email,
+            "amount":str(validated_amount),
+            "authorization_code":auth_code
+        }
+
+        return self.make_request("POST",endpoint=endpoint,data=data)
+
+    def view_transaction_timeline(self,tran_id_reference):
+        endpoint = f"/transaction/timeline/{tran_id_reference}"
+        return self.make_request("GET",endpoint=endpoint)
     
+    def transaction_totals(self):
+        endpoint = '/transaction/totals'
+        return self.make_request("GET",endpoint=endpoint)
+    
+    def export_transaction(self):
+        endpoint = '/transaction/export'
+        return self.make_request("GET",endpoint=endpoint)
+    
+
+    def partial_debit(self,auth_code:str,email:EmailStr,amount:float):
+        endpoint = '/transaction/partial_debit'
+
+        validated_amount = validate_amount(amount=amount)*100
+
+        data = {
+            "authorization_code":auth_code,
+            "Currency":"GHS",
+            "email":email,
+            "amount" : str(validated_amount)
+        }
+
+        return self.make_request("POST",endpoint=endpoint,data=data)
